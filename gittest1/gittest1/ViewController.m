7ef8f71bd2d4c61824366599d3beb56da26f173e
Silentnoisy <silentnoisy@icloud.com>
2017-08-28 22:24:04 +0800

    init

---
+++ gittest1/gittest1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  gittest1
+//
+//  Created by ZhangHaibo on 17/8/28.
+//  Copyright © 2017年 ZhangHaibo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
